680d33940b01831306a93835c235833ae5f0997d
Urch1234 <iernest61@gmail.com>
2022-09-08 22:48:06 -0700

    type size5

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
- *
- * Descripton: using the main function
- * this prints prints the size of various types on the computer it is compiled
- * Return: 0
- */
+* main - print the string in the printf function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 {
 	char c;
 	int i;
-	long j;
-	long long k;
+	long li;
+	long long lli;
 	float f;
 
-	printf("Size of a char: %1d byte(s)\n", sizeof(c));
-	printf("Size of an int: %1d byte(s)\n", sizeof(i));
-	printf("Size of a long int: %1d byte(s)\n", sizeof(j));
-	printf("Size of a long long int: %1d byte(s)\n", sizeof(k));
-	printf("Size of a float: %1d byte(s)\n", sizeof(f));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
+}
