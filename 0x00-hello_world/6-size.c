0e56708e5f8e4baa30924104baa2a8dc77ea5b0e
Urch1234 <iernest61@gmail.com>
2022-09-08 22:42:47 -0700

    type size4

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,13 +11,13 @@ int main(void)
 {
 	char c;
 	int i;
-	long li;
-	long long lli;
+	long j;
+	long long k;
 	float f;
 
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	printf("Size of a char: %1d byte(s)\n", sizeof(c));
+	printf("Size of an int: %1d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %1d byte(s)\n", sizeof(j));
+	printf("Size of a long long int: %1d byte(s)\n", sizeof(k));
+	printf("Size of a float: %1d byte(s)\n", sizeof(f));
 	return (0);
