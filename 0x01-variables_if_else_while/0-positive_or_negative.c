4b0501ec1e92e9b755106c9882814b28bf715c1b
Urch1234 <iernest61@gmail.com>
2022-09-09 21:10:00 -0700

    Random integers

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints if the number is either positive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Programing is positive, zero, or negative
+ * Return: 0
+ */
+
+/* betty style doc for function main goes here */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%i is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%i is negative\n", n)
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Program to assign a random number to the variable n each time it is excuted.
