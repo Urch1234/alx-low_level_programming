5b06c373a1d341bff6eeea4004e5ca2cb93d4ee6
Urch1234 <iernest61@gmail.com>
2022-09-11 13:31:50 -0700

    combination of the digits between 0 and 100 update1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,15 +9,18 @@ int main(void)
 {
 int i, p;
 
-for (i = '0'; i < '100'; i++)
+for (i = 0; i < 100; i++)
 {
-for (p = '0'; p < '100'; p++)
+for (p = 0; p < 100; p++)
 {
 if (p > i)
 {
-	putchar(i / '10');
-	putchar(1 % '10');
-	if (i != '98');
+	putchar((i / 10) + '0');
+	putchar((1 % 10) + '0');
+	putchar(' ');
+	putchar((p / 10) + '0');
+	putchar((p % 10) + '0');
+	if (i != 98);
 	{
 		putchar(',');
 		putchar(' ');
