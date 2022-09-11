991faa0c312851cfbc88e94ce1728bba54644c98
Urch1234 <iernest61@gmail.com>
2022-09-10 22:16:01 -0700

    digit combination3

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,6 +3,7 @@
  * main - print all possible combination of single-digit number
  * Description: using main function
  * print in ascending order "0,1,2,3,4,5,6,7,8,9"
+ * Return 0
  */
 int main(void)
 {
