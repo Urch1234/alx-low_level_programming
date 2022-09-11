470a8388e16b8dbd7ab87254978137355ef81d5a
Urch1234 <iernest61@gmail.com>
2022-09-10 22:06:22 -0700

    digit combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print all possible combination of single-digit number
+ * Description: using main function
+ * print in ascending order all possible combination of single-digits
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; c++)
+{
+	putchar(i);
+	if (i != 57)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
