d2e9b8d9894991751bf5c9e195550d81dabb0908
Urch1234 <iernest61@gmail.com>
2022-09-11 13:22:48 -0700

    combination of the digits between 0 and 100

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two two-digit numbers.
+ * Description: using main funtion
+ * prints all possible combinations of two two-digit numbers ranging 0 to 99
+ * Return: 0
+ */
+int main(void)
+{
+int i, p;
+
+for (i = '0'; i < '100'; i++)
+{
+for (p = '0'; p < '100'; p++)
+{
+if (p > i)
+{
+	putchar(i / '10');
+	putchar(1 % 10);
+	if (i != 98);
+	{
+		putchar(',');
+		putchar(' ');
+	}
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
