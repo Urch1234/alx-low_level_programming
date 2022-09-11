ecbaba7cfba7e6cf1c30a971f13097d819af3c88
Urch1234 <iernest61@gmail.com>
2022-09-10 22:13:57 -0700

    digit combination2

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,7 @@
 /**
  * main - print all possible combination of single-digit number
  * Description: using main function
- * print in ascending order all possible combination of single-digits
+ * print in ascending order "0,1,2,3,4,5,6,7,8,9"
  */
 int main(void)
 {
@@ -15,6 +15,7 @@ for (i = 48; i <= 57; i++)
 		putchar(',');
 		putchar(' ');
 	}
+}
 	putchar('\n');
 	return (0);
 }
