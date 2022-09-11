dbc52d7ded8528746eaeb27d86c524dc6f8e8072
Urch1234 <iernest61@gmail.com>
2022-09-10 20:10:47 -0700

    alphabet in lowercase, except e&q update2

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-	char ch;
+char ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-	if (ch != 'e' && ch != 'q')
-	{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'e' && ch != 'q')
+{
 	putchar(ch);
-	}
-	}
-	putchar('\n');
-	return (0);
+}
+}
+putchar('\n');
+return (0);
 }
