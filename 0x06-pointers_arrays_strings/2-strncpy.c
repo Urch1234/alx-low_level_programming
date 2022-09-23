202d799cf79d8ae5395a9b965355c3367678559d
Urch1234 <iernest61@gmail.com>
2022-09-22 19:17:07 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,7 +8,9 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	for (int i = 0; i < n && src[i] != '\0'; i++)
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
 	for ( ; i < n; i++)
