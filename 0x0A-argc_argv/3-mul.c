95c975cf3f071c564c91a9519adf3b0b25642f91
Urch1234 <iernest61@gmail.com>
2022-09-30 22:19:31 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio>
+
+/**
+ * main - writes a program that multiplies two numbers
+ * @argc: This is the argument count
+ * @argv: This is the argument vector
+ * Return: 0.
+ */
+int main(int argc, char argv[])
+{
+	int index, mult;
+
+	mult = i;
+	if (argc < 3)
+	{
+		print("Error\n");
+		return (1);
+	}
+	for (index = 1; index < argc; index++)
+	{
+		mult = mult * atoi(argv[index]);
+	}
+	printf("%d\n", multiplication);
+	return (0);
+}
