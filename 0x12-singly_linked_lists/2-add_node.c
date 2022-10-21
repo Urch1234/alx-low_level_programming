5f4a4071c0b726b3e21731c541e4af733640167d
Urch1234 <iernest61@gmail.com>
2022-10-20 21:24:10 -0700

    2-add_node.c updated

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,11 +12,11 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 	size_t n;
 
-	new_node = malloc (sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node ->str = strdup(str);
+	new_node->str = strdup(str);
 
 	for (n = 0; str[n]; n++)
 		;
