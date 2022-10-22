cf068984eec026b8cbbc277e74575dadd89495e7
Urch1234 <iernest61@gmail.com>
2022-10-21 19:25:23 -0700

    100-first.c update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,8 +5,8 @@
  * Return: nothing
  */
 
-void _attribute_((constructor)) first()
+void __attribute__((constructor)) first()
 {
-	printf("You're beat! and yet, you must allow");
-	printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
