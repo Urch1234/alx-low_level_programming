35c5dfde93e1f21f1dfb005c40100baa978d21d4
Urch1234 <iernest61@gmail.com>
2022-10-21 18:13:03 -0700

    3-add_node_end.c update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,7 +8,7 @@
  * Return: address of the head
  */
 
-list_s *add_node_end(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *current_node;
 	size_t n;
