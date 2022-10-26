de5802e3a91297d9b70a8c3dc6ffa8e8cf73f7b8
Urch1234 <iernest61@gmail.com>
2022-10-25 20:49:41 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,7 +7,7 @@
  *
  * Return: address odvthe newly added node
  */
-listint_t *add_nodeint_end(listint_int **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node;
 	listint_t *cursor = *head;
