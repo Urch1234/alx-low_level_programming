91e588be11bf6faf3a525608d325e4c2ab2aa6a0
Urch1234 <iernest61@gmail.com>
2022-11-02 19:58:12 -0700

    1-create_file.c revisited

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,11 +2,10 @@
 
 /**
  * create_file - creates a file
- * @filename: filename
+ * @filename: filename.
  * @text_content: content written in the file
  * Return: 1 if successful, -1 if it fails
  */
-
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
@@ -15,13 +14,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREATE | O_WRONLY | O_TRUNC, 0600);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
 	if (!text_content)
-		text_content = " ";
+		text_content = "";
 
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
