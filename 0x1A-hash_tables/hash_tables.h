7fdc0f81409eef87dbffa26d04ea5072075b5dc7
Urch1234 <iernest61@gmail.com>
2023-01-10 00:15:51 -0800

    All task on Hash

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,5 @@
+0-hash_table_create.c
+
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
@@ -82,4 +84,5 @@ void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
 
-#endif /* HASH_TABLES_H *
+#endif /* HASH_TABLES_H */
+
