33c6070603a1b31c86e6809b877b8c3accaa67ef
Ihezie Ernest Uchenna <iernest61@gmail.com>
2023-08-15 09:14:59 -0700

    0x02. C - Functions, nested loops mandatory task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - A function that adds two integers and returns the result.
+ * @num1: The fisrt integer
+ * @num2: The second intger
+ * Return: The result of the addition
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
