c3151f6d9fed749839ae48e86c05fc2dd974293f
Ihezie Ernest Uchenna <iernest61@gmail.com>
2023-08-17 10:10:38 -0700

    First 6 tasks

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Function that prints all numbers from 0 to 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+}
