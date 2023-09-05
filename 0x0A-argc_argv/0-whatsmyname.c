f9662bd4d172b3ce5bee617b99aadb0b92751dc1
Ihezie Ernest Uchenna <iernest61@gmail.com>
2023-09-05 10:56:30 -0700

    ALl task done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints its name,followed by a new line.
+ * @argc: This is the argument count
+ * @argv: This is the argument vector
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
