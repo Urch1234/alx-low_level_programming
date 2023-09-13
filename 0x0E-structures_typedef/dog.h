e76a84ec6e0f35a2fc4253941644e386f91abf11
Ihezie Ernest Uchenna <iernest61@gmail.com>
2023-09-12 22:02:56 -0700

    dog.h amended

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef STRTYP
-#define STRTYP
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - structure of a dog.
@@ -21,4 +21,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /*STRTYP*/
+#endif
