af09337137b46598002cf2380760c990ee64a8ba
Ihezie Ernest Uchenna <iernest61@gmail.com>
2023-12-14 19:58:26 -0800

    0-binary_to_uint.c and README

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+usigned int binary_to_unit(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+int _ptchar(char c);
+
+#endif
