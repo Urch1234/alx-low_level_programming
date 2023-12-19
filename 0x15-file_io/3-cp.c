e7c077c384fe9a2e3c7016176cb8cc496886831c
Ihezie Ernest Uchenna <iernest61@gmail.com>
2023-12-18 20:51:13 -0800

    some code adjustment

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -63,7 +63,7 @@ int main(int argc, char *argv[])
 	err_close = close(file_to);
 	if (err_close == -1)
 	{
-		dprinntf(STDERR_FILENO, "Error: can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
